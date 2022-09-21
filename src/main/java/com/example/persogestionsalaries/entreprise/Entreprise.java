2bc92e88ac92008a063e2d39a3cd6bf2d7865ff3
Marc Annezo <marc.annezo@gmail.com>
2022-09-21 13:22:29 +0200

    MAP ORM

--- src/main/java/com/example/persogestionsalaries/entreprise/Entreprise.java
+++ src/main/java/com/example/persogestionsalaries/entreprise/Entreprise.java
@@ -3,11 +3,25 @@ package com.example.persogestionsalaries.entreprise;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
 @Setter
+
+@Entity
+@Table
 public class Entreprise {
+    @Id
+    @SequenceGenerator(
+            name = "sequence_entreprise",
+            sequenceName = "sequence_entreprise",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "sequence_entreprise"
+    )
     private Long id;
     private Long immatriculationRCS;
     private String nom;
